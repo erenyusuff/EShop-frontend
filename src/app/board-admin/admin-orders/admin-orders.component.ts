8168f34981faa368d0317625401b626f0c07e058
Yusuf Eren <yusuf.er48en@gmail.com>
2024-01-23 13:25:27 +0300

    Drag & Drop orders created but not updateable now

--- src/app/board-admin/admin-orders/admin-orders.component.ts
+++ src/app/board-admin/admin-orders/admin-orders.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import {
   CdkDrag,
   CdkDragDrop,
@@ -11,6 +11,8 @@ import {Order} from "../../orders/order.model";
 import {Cart} from "../../cart/cart.model";
 import {OrdersService} from "../../_services/orders.service";
 import {CommonModule} from "@angular/common";
+import {group} from "@angular/animations";
+import _ from "lodash";
 
 /**
  * @title Drag&Drop connected sorting group
@@ -23,27 +25,38 @@ import {CommonModule} from "@angular/common";
   imports: [CdkDrag, CdkDropList, CdkDropListGroup, CommonModule]
 })
 export class AdminOrdersComponent {
-  order: Order[]
-  cart: Cart
-  paid: string
-  prepare: string
+  order: Order[];
+  cart: Cart;
+  paid: string;
+  grouped: any;
 
-  constructor(private orderService: OrdersService) {
+  constructor(
+    private orderService: OrdersService,
+    private cdRef: ChangeDetectorRef
+  ) {
   }
+
+  statuses = ['paid', 'preparing', 'waiting']
 
   ngOnInit(): void {
 
-    this.orderService.getAllOrders().subscribe((result) => {
+    this.orderService.getAllOrders().subscribe((result): any => {
       if (result) {
         this.order = result;
-        console.log(result)
 
-
+        let grouped: any = this.order.reduce(
+          (result: any, currentValue: any) => {
+            (result[currentValue['status']] = result[currentValue['status']] || []).push(currentValue);
+             return result;
+          }, {});
+        // const grouped = (_.groupBy(this.order, 'status'))
+       this.grouped = grouped;
       }
     })
   }
 
   drop(event: CdkDragDrop<any>) {
+    console.log('event', event);
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -55,4 +68,12 @@ export class AdminOrdersComponent {
       );
     }
   }
+
+  changeStatus() {
+
+  }
+
+  protected readonly group = group;
+  protected readonly JSON = JSON;
+  protected readonly Object = Object;
 }
