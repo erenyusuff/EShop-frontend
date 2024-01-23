b454c30bdbefa5ad2be428402ebf91374ceab8cc
Yusuf Eren <yusuf.er48en@gmail.com>
2024-01-23 17:32:54 +0300

    orders were grouped according their status in order admin page

--- src/app/admin/product-admin/product-admin.component.ts
+++ src/app/admin/product-admin/product-admin.component.ts
@@ -10,21 +10,25 @@ import {ProductAdminService} from "../../_services/product-admin.service";
   styleUrls: ['./product-admin.component.css']
 })
 export class ProductAdminComponent implements OnInit {
-  modal='modal';
+  modal = 'modal';
   modal2 = 'modal2';
   name = 'Angular';
   @ViewChild('myModal') myModal: ElementRef;
   productForm = new FormGroup({
-    productName : new FormControl(''),
-    price : new FormControl(''),
-    stock : new FormControl(''),
-    description : new FormControl(''),
-    img : new FormControl(''),
-    category : new FormControl(''),
-    id : new FormControl(''),
-    createdAt : new FormControl(''),
-    updatedAt : new FormControl(''),
+    productName: new FormControl(''),
+    price: new FormControl(''),
+    stock: new FormControl(''),
+    description: new FormControl(''),
+    img: new FormControl(''),
+    category: new FormControl(''),
+    id: new FormControl(''),
+    createdAt: new FormControl(''),
+    updatedAt: new FormControl(''),
   });
+  updateProductForm = new FormGroup({
+    stock: new FormControl(''),
+  });
+
   Product: product | any
 
   constructor(private productAdminService: ProductAdminService, private modalService: NgbModal) {
@@ -38,6 +42,8 @@ export class ProductAdminComponent implements OnInit {
       }
     })
   }
+
+
   addProduct(): void {
     const {productName, price, stock, description, img, category, id, createdAt, updatedAt} = this.productForm.value;
     this.productAdminService.addProduct({
@@ -52,37 +58,38 @@ export class ProductAdminComponent implements OnInit {
       updatedAt
     }).subscribe()
   }
+
   deleteProduct(id: any): void {
     this.productAdminService.deleteProducts(id).subscribe()
   }
+
   refresh(): void {
     window.location.reload();
   }
+
   updateProduct(id: number): void {
-    const {productName, price, stock, description, img, category, createdAt, updatedAt} = this.productForm.value;
+    const {stock} = this.updateProductForm.value;
     this.productAdminService.updateProducts({
-      productName,
-      price,
       stock,
-      description,
-      img,
-      category
     }, id).subscribe()
   }
 
   openModal(inp: string) {
     console.log(inp);
-    this.modal='modal-open';
+    this.modal = 'modal-open';
   }
-  closeModal(){
-    this.modal='modal';
+
+  closeModal() {
+    this.modal = 'modal';
   }
+
   openModal2(inp: string) {
     console.log(inp);
-    this.modal2='modal-open';
+    this.modal2 = 'modal-open';
   }
-  closeModal2(){
-    this.modal2='modal';
+
+  closeModal2() {
+    this.modal2 = 'modal';
   }
 
   protected readonly ProductAdminService = this.productAdminService;
